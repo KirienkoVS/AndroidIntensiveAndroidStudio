30c10b3630043d564f2dfa92988579f132961ee3
KirienkoVS <kirienkov@yandex.ru>
2022-02-11 22:01:03 +0300

    Added click listeners for TOAST (shows toast message) and COUNT (increments count and displays it in TextView) buttons

--- app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.androidintensiveandroidstudio
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private var count: Int = 0
+    private lateinit var countView: TextView
+    private lateinit var toastButton: Button
+    private lateinit var contButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "Happy Birthday!")
+        countView = findViewById(R.id.show_count)
+        toastButton = findViewById(R.id.button_toast)
+        contButton = findViewById(R.id.button_count)
+
+        toastButton.setOnClickListener {
+            Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
+        }
+
+        contButton.setOnClickListener {
+            count++
+            countView.text = count.toString()
+        }
     }
 }
