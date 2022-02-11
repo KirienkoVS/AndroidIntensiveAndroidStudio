22f2f85c1edb04d7ee8171859cab84cb2911aa76
KirienkoVS <kirienkov@yandex.ru>
2022-02-11 19:31:16 +0300

    Add Log statement in MainActivity class

--- app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.androidintensiveandroidstudio
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "Hello World")
     }
 }
