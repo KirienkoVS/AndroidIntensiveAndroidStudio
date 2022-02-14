dc7189130d84c7dd00348cf7b873bb732dadeb9b
KirienkoVS <kirienkov@yandex.ru>
2022-02-14 23:15:01 +0300

    Creates the TwoActivities project

--- app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.androidintensiveandroidstudio
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "Happy Birthday!")
+        val sendButton = findViewById<Button>(R.id.button_main)
+        sendButton.setOnClickListener {
+            Log.d(TAG, "Button clicked!")
+        }
     }
 }
