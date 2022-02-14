4f6574b3a3e8d2916c48ee6f455727bb15844fd4
KirienkoVS <kirienkov@yandex.ru>
2022-02-15 00:50:22 +0300

    Returns data back to the main Activity (deprecated version)

--- app/src/main/java/com/example/androidintensiveandroidstudio/Constants.kt
+++ app/src/main/java/com/example/androidintensiveandroidstudio/Constants.kt
@@ -2,3 +2,5 @@ package com.example.androidintensiveandroidstudio
 
 const val TAG = "MainActivity"
 const val EXTRA_MESSAGE = "com.example.androidintensiveandroidstudio.extra.MESSAGE"
+const val EXTRA_REPLY = "com.example.androidintensiveandroidstudio.extra.REPLY"
+const val TEXT_REQUEST = 1
--- app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
@@ -3,27 +3,51 @@ package com.example.androidintensiveandroidstudio
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sendButton: Button
     private lateinit var messageEditText: EditText
+    private lateinit var replyHeadTextView: TextView
+    private lateinit var replyTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         messageEditText = findViewById(R.id.editText_main)
-        sendButton = findViewById<Button>(R.id.button_main)
+        sendButton = findViewById(R.id.button_main)
+        replyHeadTextView = findViewById(R.id.text_header_reply)
+        replyTextView = findViewById(R.id.text_message_reply)
+
         sendButton.setOnClickListener {
             Log.d(TAG, "Button clicked!")
             val intent = Intent(this, SecondActivity::class.java)
             val message = messageEditText.text.toString()
             intent.putExtra(EXTRA_MESSAGE, message)
-            startActivity(intent)
+            startActivityForResult(intent, TEXT_REQUEST)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                val reply = data?.getStringExtra(EXTRA_REPLY)
+
+                replyHeadTextView.visibility = View.VISIBLE
+
+                replyTextView.apply {
+                    text = reply
+                    visibility = View.VISIBLE
+                }
+            }
         }
     }
 }
