bfe09e92f17e2e356eb1503e09413264f9dc3f53
KirienkoVS <kirienkov@yandex.ru>
2022-02-16 00:00:27 +0300

    Coding challenge

--- app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
@@ -3,41 +3,27 @@ package com.example.androidintensiveandroidstudio
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var sendButton: Button
-    private lateinit var messageEditText: EditText
-    private lateinit var replyHeadTextView: TextView
-    private lateinit var replyTextView: TextView
+    private lateinit var textView1: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(MAIN_ACTIVITY, "-------")
-        Log.d(MAIN_ACTIVITY, "onCreate")
-
-        messageEditText = findViewById(R.id.editText_main)
-        sendButton = findViewById(R.id.button_main)
-        replyHeadTextView = findViewById(R.id.text_header_reply)
-        replyTextView = findViewById(R.id.text_message_reply)
+        val addButton = findViewById<Button>(R.id.button_main)
+        textView1 = findViewById(R.id.text_view_1)
 
         if (savedInstanceState != null) {
-            val isVisible = savedInstanceState.getBoolean("reply_visible")
-            if (isVisible) {
-                replyHeadTextView.apply {
-                    visibility = View.VISIBLE
-                    text = savedInstanceState.getString("reply_text")
-                }
-                replyTextView.visibility = View.VISIBLE
+            textView1.apply {
+                visibility = View.VISIBLE
+                text = savedInstanceState.getString("item")
             }
         }
 
@@ -45,58 +31,22 @@ class MainActivity : AppCompatActivity() {
             ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val reply = result.data?.getStringExtra(EXTRA_REPLY)
-                replyHeadTextView.visibility = View.VISIBLE
-                replyTextView.apply {
+                textView1.apply {
                     text = reply
                     visibility = View.VISIBLE
                 }
             }
         }
 
-        sendButton.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            val message = messageEditText.text.toString()
-            intent.putExtra(EXTRA_MESSAGE, message)
-            startActivityForResult.launch(intent)
+        addButton.setOnClickListener {
+            startActivityForResult.launch(Intent(this, SecondActivity::class.java))
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        if (replyHeadTextView.visibility == View.VISIBLE) {
-            outState.putBoolean("reply_visible", true)
-            outState.putString("reply_text", replyTextView.text.toString())
+        if (textView1.text.isNotBlank()) {
+            outState.putString("item", textView1.text.toString())
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(MAIN_ACTIVITY, "onStart")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(MAIN_ACTIVITY, "onPause")
-        messageEditText.text.clear()
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(MAIN_ACTIVITY, "onRestart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(MAIN_ACTIVITY, "onResume")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(MAIN_ACTIVITY, "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(MAIN_ACTIVITY, "onDestroy")
     }
 }
--- app/src/main/java/com/example/androidintensiveandroidstudio/SecondActivity.kt
+++ app/src/main/java/com/example/androidintensiveandroidstudio/SecondActivity.kt
@@ -2,67 +2,46 @@ package com.example.androidintensiveandroidstudio
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
-
-    private lateinit var replyButton: Button
-    private lateinit var messageEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        messageEditText = findViewById(R.id.editText_second)
-        replyButton = findViewById(R.id.button_second)
+        val cheeseButton = findViewById<Button>(R.id.cheese_button)
+        val riceButton = findViewById<Button>(R.id.rice_button)
+        val applesButton = findViewById<Button>(R.id.apples_button)
+        val orangesButton = findViewById<Button>(R.id.oranges_button)
+        val potatoButton = findViewById<Button>(R.id.potato_button)
+        val breadButton = findViewById<Button>(R.id.bread_button)
+        val juiceButton = findViewById<Button>(R.id.juice_button)
+        val tomatoesButton = findViewById<Button>(R.id.tomatoes_button)
+        val bananasButton = findViewById<Button>(R.id.bananas_button)
+        val saladButton = findViewById<Button>(R.id.salad_button)
 
-        val intent = intent
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val buttonsList = listOf<Button>(
+            cheeseButton,
+            riceButton,
+            applesButton,
+            orangesButton,
+            potatoButton,
+            breadButton,
+            juiceButton,
+            tomatoesButton,
+            bananasButton,
+            saladButton
+        )
 
-        val textView = findViewById<TextView>(R.id.text_message)
-        textView.text = message
-
-        replyButton.setOnClickListener {
-            val reply = messageEditText.text.toString()
-            val replyIntent = Intent()
-            replyIntent.putExtra(EXTRA_REPLY, reply)
-            setResult(RESULT_OK, replyIntent)
-            Log.d(SECOND_ACTIVITY, "End SecondActivity")
-            finish()
+        for (button in buttonsList) {
+            button.setOnClickListener {
+                val item = button.text
+                val replyIntent = Intent().putExtra(EXTRA_REPLY, item)
+                setResult(RESULT_OK, replyIntent)
+                finish()
+            }
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(SECOND_ACTIVITY, "onStart")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(SECOND_ACTIVITY, "onPause")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(SECOND_ACTIVITY, "onRestart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(SECOND_ACTIVITY, "onResume")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(SECOND_ACTIVITY, "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(SECOND_ACTIVITY, "onDestroy")
     }
 }
