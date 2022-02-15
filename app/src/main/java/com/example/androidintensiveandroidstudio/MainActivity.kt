96572aa5b8a4bbc7300b1c03ea96573ea51cb16b
KirienkoVS <kirienkov@yandex.ru>
2022-02-16 01:35:42 +0300

    Implements the Open Website button

--- app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.androidintensiveandroidstudio
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "MainActivity"
@@ -12,5 +18,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         title = "Implicit intents"
+
+        val openWebSiteButton = findViewById<Button>(R.id.open_website_button)
+        val webSiteEditText = findViewById<TextView>(R.id.website_edittext)
+
+        openWebSiteButton.setOnClickListener {
+            val url = webSiteEditText.text.toString()
+            val webPage = Uri.parse(url)
+            val intent = Intent(Intent.ACTION_VIEW, webPage)
+
+            try {
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                Log.e(TAG, "Application nt found")
+            }
+        }
     }
 }
