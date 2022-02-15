08df9848146fc91e11e6e2854aa50e67fe90ed0a
KirienkoVS <kirienkov@yandex.ru>
2022-02-16 01:16:44 +0300

    Creates the project and layout

--- app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.androidintensiveandroidstudio
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "Happy Birthday!")
+        title = "Implicit intents"
     }
 }
