e8ddc6e792aa7bbfbdbca3dcfb462c9fc57109bf
KirienkoVS <kirienkov@yandex.ru>
2022-02-16 18:21:48 +0300

    Receives an implicit Intent

--- app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.example.androidintensiveandroidstudio
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ShareCompat
 
 private const val TAG = "MainActivity"
 
@@ -20,47 +14,13 @@ class MainActivity : AppCompatActivity() {
 
         title = "Implicit intents"
 
-        val openWebSiteButton = findViewById<Button>(R.id.open_website_button)
-        val webSiteEditText = findViewById<TextView>(R.id.website_edittext)
+        val intent = intent
+        val uri = intent.data
 
-        val openLocationButton = findViewById<Button>(R.id.open_location_button)
-        val locationEditText = findViewById<TextView>(R.id.location_edittext)
-
-        val shareTextButton = findViewById<Button>(R.id.share_text_button)
-        val shareEditText = findViewById<TextView>(R.id.share_edittext)
-
-        openWebSiteButton.setOnClickListener {
-            val url = webSiteEditText.text.toString()
-            val webPage = Uri.parse(url)
-            val intent = Intent(Intent.ACTION_VIEW, webPage)
-
-            try {
-                startActivity(intent)
-            } catch (e: ActivityNotFoundException) {
-                Toast.makeText(this, "Application not found", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        openLocationButton.setOnClickListener {
-            val location = locationEditText.text.toString()
-            val addressUri = Uri.parse("geo:0,0?q=$location")
-            val intent = Intent(Intent.ACTION_VIEW, addressUri)
-
-            try {
-                startActivity(intent)
-            } catch (e: ActivityNotFoundException) {
-                Toast.makeText(this, "Application not found", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        shareTextButton.setOnClickListener {
-            val text = shareEditText.text.toString()
-            val mimeType = "text/plain"
-            ShareCompat.IntentBuilder(this)
-                .setType(mimeType)
-                .setChooserTitle(getString(R.string.chooser_title))
-                .setText(text)
-                .startChooser()
+        if (uri != null) {
+            val uriString = "URI: $uri"
+            val textView = findViewById<TextView>(R.id.text_uri_message)
+            textView.text = uriString
         }
     }
 }
