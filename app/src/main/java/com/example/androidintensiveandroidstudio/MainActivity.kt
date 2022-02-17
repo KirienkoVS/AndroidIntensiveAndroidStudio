cad2714aafc6a7f12927d663ebe49fb9f94297fc
KirienkoVS <kirienkov@yandex.ru>
2022-02-18 01:26:51 +0300

    Fixes Coding challenge problem

--- app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/androidintensiveandroidstudio/MainActivity.kt
@@ -1,38 +1,58 @@
 package com.example.androidintensiveandroidstudio
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-
-private const val TAG = "MainActivity"
+import androidx.core.view.iterator
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var textView: TextView
-    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val countButton = findViewById<Button>(R.id.button_main)
-        textView = findViewById(R.id.count_text_view)
+        val addButton = findViewById<Button>(R.id.button_main)
+        val constraintLayout = findViewById<ViewGroup>(R.id.constraint_layout)
 
-        countButton.setOnClickListener {
-            count++
-            textView.text = count.toString()
+        val textViewsList = mutableListOf<TextView>()
+        for (view in constraintLayout) {
+            if (view is TextView) {
+                textViewsList.add(view)
+            }
         }
-    }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(TAG, count)
-    }
+        val startActivityForResult = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val reply = result.data?.getStringExtra(EXTRA_REPLY)
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        count = savedInstanceState.getInt(TAG)
-        textView.text = count.toString()
+                if (textViewsList.first().text.isBlank()) {
+                    textViewsList.first().apply {
+                        text = reply
+                        visibility = View.VISIBLE
+                    }
+                } else {
+                    val emptyTextView = textViewsList.first { it.text.isBlank() }
+                    emptyTextView.apply {
+                        text = reply
+                        visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
+
+        addButton.setOnClickListener {
+            val itemsList = textViewsList.filter { it.text.isNotBlank() }
+            if (itemsList.size > 10) {
+                recreate()
+            }
+            startActivityForResult.launch(Intent(this, SecondActivity::class.java))
+        }
     }
 }
